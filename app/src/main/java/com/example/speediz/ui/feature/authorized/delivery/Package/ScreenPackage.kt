8685023a8e492daa3268752ec54876b44ba669a7
MrHeak <heakschools@gmail.com>
2025-10-20 18:47:24 +0700

    package

--- app/src/main/java/com/example/speediz/ui/feature/authorized/delivery/Package/ScreenPackage.kt
+++ app/src/main/java/com/example/speediz/ui/feature/authorized/delivery/Package/ScreenPackage.kt
@@ -1,8 +1,13 @@
 package com.example.speediz.ui.feature.authorized.delivery.Package
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
 fun ScreenPackage() {
-
+    Box(
+        modifier = Modifier.fillMaxSize()
+    )
 }
